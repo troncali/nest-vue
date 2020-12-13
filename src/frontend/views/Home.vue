13e6796bb8d484905151a14e3b0c4fbd6cf19f91
Troncali <7217131+troncali@users.noreply.github.com>
2020-12-13 00:03:18 -0800

    Default Nest and Vue files

---
+++ src/frontend/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	</div>
+</template>
+
+<script lang="ts">
+	import { Options, Vue } from "vue-class-component";
+	import HelloWorld from "@/frontend/components/HelloWorld.vue";
+
+	@Options({
+		components: {
+			HelloWorld
+		}
+	})
+	export default class Home extends Vue {}
+</script>
