66735495e25cbbce97b55e1cdd1cdee867d0bbaf
Troncali <7217131+troncali@users.noreply.github.com>
2021-03-18 23:23:32 -0700

    Add GraphQL configuration

--- src/backend/config/graphql/config.types.ts
+++ src/backend/config/graphql/config.types.ts
@@ -1,8 +1,8 @@
 /** GraphQL configuration properties for intellisense. */
 interface GqlOptionTypes {
-	// Full property path is required for nested properties.
-	"gql.autoSchemaFile": boolean;
+	"gql.autoSchemaFile": string;
 	"gql.sortSchema": boolean;
+	"gql.path": string;
 }
 
 export { GqlOptionTypes };
