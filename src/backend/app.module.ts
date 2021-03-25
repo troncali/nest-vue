b157c230ec0f2d25fa245fbab8db7c3493652cc5
Troncali <7217131+troncali@users.noreply.github.com>
2021-03-25 00:06:09 -0700

    Revise GraphQL path; implement base service and resolver for dynamic GraphQL db queries; initial Users model, repository, service, and resolver.

--- src/backend/app.module.ts
+++ src/backend/app.module.ts
@@ -9,9 +9,11 @@ import { DbConfigModule } from "./config/db/config.module";
 import { DefaultDbConfigService } from "./config/db/default-postgres/config.service";
 import { GqlConfigModule } from "./config/graphql/config.module";
 import { GqlConfigService } from "./config/graphql/config.service";
+import { UsersModule } from "./models/users/users.module";
 @Module({
 	imports: [
 		AppConfigModule,
+		// TODO: put db and gql in provider(s)
 		GraphQLModule.forRootAsync({
 			imports: [GqlConfigModule],
 			useExisting: GqlConfigService
@@ -19,7 +21,8 @@ import { GqlConfigService } from "./config/graphql/config.service";
 		TypeOrmModule.forRootAsync({
 			imports: [DbConfigModule],
 			useExisting: DefaultDbConfigService
-		})
+		}),
+		UsersModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
