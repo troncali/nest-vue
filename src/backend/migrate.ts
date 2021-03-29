ab3285cfeb490d4924edf533b265e1dba226d919
Troncali <7217131+troncali@users.noreply.github.com>
2021-03-28 18:46:40 -0700

    Implement Migrator and initial migration for User entity

---
+++ src/backend/migrate.ts
@@ -0,0 +1,52 @@
+import { NestFactory } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
+
+import { Migrator } from "./providers/migrations/migrators";
+import { MigrationProvider } from "./providers/migrations/migration.provider";
+
+/**
+ * Start NestJS instance for migrations.
+ */
+async function bootstrap() {
+	const app = await NestFactory.createApplicationContext(Migrator);
+	const migrator = app.get(MigrationProvider);
+	const arg = process.argv.slice(2);
+
+	// Use command line argument to call `run` or `undo` migration methods.
+	const handle = async (
+		methodName: "run" | "undo",
+		startMessage: string,
+		failMessage: string,
+		completeMessage: string
+	) => {
+		Logger.debug(`${startMessage}`);
+		await migrator[methodName]().catch((error) => {
+			Logger.error(`${failMessage}`);
+			throw error;
+		});
+		Logger.debug(`${completeMessage}`);
+	};
+
+	switch (arg[0]) {
+		case "undo":
+			await handle(
+				"undo",
+				"Reverting last migration",
+				"Rollback failed",
+				"Rollback complete"
+			);
+			break;
+
+		case "run":
+		default:
+			await handle(
+				"run",
+				"Running migrations",
+				"Migrations failed",
+				"Migrations complete"
+			);
+	}
+
+	app.close();
+}
+bootstrap();
---
+++ src/backend/providers/migrations/migration.provider.ts
@@ -0,0 +1,29 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { Connection } from "typeorm";
+
+/**
+ * Migration service that dispatches database actions.
+ *
+ * @class
+ */
+@Injectable()
+export class MigrationProvider {
+	constructor(private readonly connection: Connection) {}
+
+	/**
+	 * Runs all migrations created since the last run.
+	 */
+	async run() {
+		(await this.connection.showMigrations())
+			? await this.connection.runMigrations()
+			: Logger.error("No new migrations");
+	}
+
+	/**
+	 * Reverts only the very last migration file that was run. Repeat to roll
+	 * back additional files, one by one.
+	 */
+	async undo() {
+		await this.connection.undoLastMigration();
+	}
+}
