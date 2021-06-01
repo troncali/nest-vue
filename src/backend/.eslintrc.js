a3eb41a728ca102b0ed9f553b08c3aa0430706be
Troncali <7217131+troncali@users.noreply.github.com>
2021-05-31 22:26:56 -0700

    Initial conversion to Nx workspace; upgrade dependencies.

--- src/backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	extends: ["../../.eslintrc.js"],
-	rules: {
-		"@typescript-eslint/interface-name-prefix": "off",
-		"@typescript-eslint/explicit-function-return-type": "off",
-		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"@typescript-eslint/no-explicit-any": "off"
-	}
-};
