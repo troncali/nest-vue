98919536452573a7bff8728c7a94829a301c3c6b
Troncali <7217131+troncali@users.noreply.github.com>
2021-05-31 22:26:56 -0700

    Initial conversion to Nx workspace; upgrade dependencies.

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,39 @@
 module.exports = {
-	projects: [
-		{
-			displayName: "backend",
-			testEnvironment: "node",
-			testMatch: [
-				"<rootDir>/**/backend/**/*.spec.[jt]s?(x)",
-				"<rootDir>/**/e2e/backend/**/*.[jt]s?(x)"
-			],
-			moduleNameMapper: {
-				"^@/(.*)$": "<rootDir>/src/$1"
-			},
-			transform: {
-				"^.+\\.(t|j)s$": "ts-jest"
-			}
-		},
-		{
-			displayName: "frontend",
-			preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-			transform: {
-				"^.+\\.vue$": "vue-jest",
-				"^.+\\.(t|j)s$": "ts-jest"
-			},
-			testMatch: [
-				"<rootDir>/**/frontend/**/*.spec.[jt]s?(x)",
-				"<rootDir>/**/e2e/frontend/**/*.[jt]s?(x)"
-			],
-			testEnvironment: "jest-environment-jsdom-fifteen",
-			globals: {
-				"vue-jest": {
-					tsConfig: "./src/frontend/tsconfig.json"
-				}
-			}
-		}
-	]
+	projects: ["<rootDir>/src/backend"]
+
+	// MOVE THIS INTO EACH PROJECT
+	// projects: [
+	// 	{
+	// 		displayName: "backend",
+	// 		testEnvironment: "node",
+	// 		testMatch: [
+	// 			"<rootDir>/**/backend/**/*.spec.[jt]s?(x)",
+	// 			"<rootDir>/**/e2e/backend/**/*.[jt]s?(x)"
+	// 		],
+	// 		moduleNameMapper: {
+	// 			"^@/(.*)$": "<rootDir>/src/$1"
+	// 		},
+	// 		transform: {
+	// 			"^.+\\.(t|j)s$": "ts-jest"
+	// 		}
+	// 	},
+	// 	{
+	// 		displayName: "frontend",
+	// 		preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+	// 		transform: {
+	// 			"^.+\\.vue$": "vue-jest",
+	// 			"^.+\\.(t|j)s$": "ts-jest"
+	// 		},
+	// 		testMatch: [
+	// 			"<rootDir>/**/frontend/**/*.spec.[jt]s?(x)",
+	// 			"<rootDir>/**/e2e/frontend/**/*.[jt]s?(x)"
+	// 		],
+	// 		testEnvironment: "jest-environment-jsdom-fifteen",
+	// 		globals: {
+	// 			"vue-jest": {
+	// 				tsConfig: "./src/frontend/tsconfig.json"
+	// 			}
+	// 		}
+	// 	}
+	// ]
 };
