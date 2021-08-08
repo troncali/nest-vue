c6ca3ae7f3da6ecf5ba1936f07fe256056319777
Troncali <7217131+troncali@users.noreply.github.com>
2021-08-07 17:41:21 -0700

    Update test dependencies

--- src/backend/auth/auth.interface.ts
+++ src/backend/auth/auth.interface.ts
@@ -1,8 +1,14 @@
+import { FastifyRequest } from "fastify";
 import { UserSession } from "../models/session/session.dto";
 
-declare module "fastify" {
-	interface FastifyRequest {
-		/** Session of the authenticated user making the request. */
-		user?: UserSession | null;
-	}
+// declare module "fastify" {
+// 	export interface FastifyRequest {
+// 		/** Session of the authenticated user making the request. */
+// 		user?: UserSession | null;
+// 	}
+// }
+
+export interface AuthRequest extends FastifyRequest {
+	/** Session of the authenticated user making the request. */
+	user?: UserSession | null;
 }
